faf86145cdfd5de9b09557643e84680e8273242f
mjaliz <mrph14@gmail.com>
2023-08-22 21:19:26 +0330

    adding more sql queries for entry

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -3,3 +3,14 @@ INSERT INTO
     entries (account_id, amount)
 VALUES
     ($1, $2) RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
+
+-- name: ListEntry :many
+SELECT * FROM entries
+WHERE account_id = $3
+ORDER BY id
+LIMIT $1
+OFFSET $2;
